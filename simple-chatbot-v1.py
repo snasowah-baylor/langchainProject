2a498850b59e578bcc106fbb5fbdc8163a829d61
Sowah, Stephen <stephen_sowah1@baylor.edu>
2026-03-21 02:26:53 -0500

    Foundation: Finished the foundation

---
+++ simple-chatbot-v1.py
@@ -0,0 +1,27 @@
+import os
+from dotenv import load_dotenv  # type: ignore
+from langchain.agents import create_agent  # type: ignore
+from langchain_google_genai import ChatGoogleGenerativeAI  # type: ignore
+
+load_dotenv()
+
+api_key = os.getenv("GEMINI_API_KEY")
+
+model = ChatGoogleGenerativeAI(model="gemini-2.5-flash", temperature=0)
+
+agent = create_agent(
+    model=model,
+    tools=[],
+    system_prompt="You are a helpful chat assistant. Be clear, concise and polite. Understand the user's intent and respond directly. Stay professional and safe. "
+    " and provide a helpful answer. If you don't know the answer, say you don't know. Stay professional and safe.",
+)
+
+result = agent.invoke(
+    {
+        "messages": [
+            {"role": "user", "content": "Explain machine learning in simple terms."}
+        ],
+    }
+)
+
+print(result)
---
+++ simple-chatbot-v2.py
@@ -0,0 +1,46 @@
+import os
+from dotenv import load_dotenv  # type: ignore
+from langchain.agents import create_agent  # type: ignore
+from langchain_google_genai import ChatGoogleGenerativeAI  # type: ignore
+
+load_dotenv()
+
+api_key = os.getenv("GEMINI_API_KEY")
+
+model = ChatGoogleGenerativeAI(model="gemini-2.5-flash", temperature=0)
+
+agent = create_agent(
+    model=model,
+    tools=[],
+    system_prompt="You are a helpful chat assistant. Be clear, concise and polite. Understand the user's intent and respond directly. Stay professional and safe. "
+    " and provide a helpful answer. If you don't know the answer, say you don't know. Stay professional and safe.",
+)
+
+
+chat_history = []
+
+print("Chat Assistant ready! type 'bye' or 'exit' to end the conversation.")
+
+while True:
+    user_input = input("You: ".strip())
+    print("\n")
+    if user_input.lower() in ["bye", "exit"]:
+        print("Assistant 🤖: Goodbye! 👋")
+        break
+
+    messages = chat_history + [{"role": "user", "content": user_input}]
+    result = agent.invoke({"messages": messages})
+
+    print(result)
+
+    try:
+        reply = result["messages"][-1].content
+    except Exception as e:
+        reply = str(e)
+
+    print(f"Assistant 🤖: {reply} \n")
+    print("-" * 60)
+
+    # update chat history
+    chat_history.append({"role": "user", "content": user_input})
+    chat_history.append({"role": "assistant", "content": reply})
